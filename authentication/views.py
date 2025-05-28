6269a275b4f6c1104e4f76260fc690c58d27c452
Brad Akemi <mirado.akemi@gmail.com>
2025-05-28 14:14:16 +0300

    Auto stash before merge of "main" and "origin/Lalia"

--- authentication/views.py
+++ authentication/views.py
@@ -1,5 +1,5 @@
 from rest_framework import status, generics, viewsets
-from rest_framework.decorators import api_view, permission_classes, action
+from rest_framework.decorators import { api }_view, permission_classes, action
 from rest_framework.permissions import IsAuthenticated, AllowAny
 from rest_framework.response import Response
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
